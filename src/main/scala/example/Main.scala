b89bf4ceb107b18233808a456dae76c1883af93e
Toby Hobson <toby.hobson@gmail.com>
2019-08-22 14:04:43 +0200

    Intro to Generic

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -1,7 +1,7 @@
 package example
 
 import java.time.LocalDate
-import shapeless.{::, HNil}
+import shapeless.{::, HNil, Generic}
 
 object Main {
 
@@ -12,26 +12,19 @@ object Main {
   type ReservationH = String :: String :: LocalDate :: LocalDate :: String :: Boolean :: Int :: Int :: String :: HNil
 
   def main(args: Array[String]): Unit = {
-    val locationH: LocationH = "Malaga Airport" :: "Malaga Airport" :: LocalDate.of(2018,8,1) :: LocalDate.of(2018,8,10) :: HNil
-    val vehicleH: VehicleH = "Economy" :: false :: 4 :: HNil
-    val driverH: DriverH = 35 :: "British" :: HNil
+    // Our case class representations of the data
+    val location = Location(pickup = "Malaga Airport", dropOff = "Malaga Airport", from = LocalDate.of(2018,8,1), to = LocalDate.of(2018,8,10))
+    val vehicle = Vehicle(vehicleCategory = "Economy", automatic = false, numDoors = 4)
+    val driver = Driver(driverAge = 35, nationality = "British")
+
+    // HList representations
+    val locationH: LocationH = Generic[Location].to(location)
+    val vehicleH: VehicleH = Generic[Vehicle].to(vehicle)
+    val driverH: DriverH = Generic[Driver].to(driver)
 
     val reservationH: ReservationH = locationH ++ vehicleH ++ driverH
-
-    reservationH match {
-      case
-        pickup ::
-        dropOff ::
-        from ::
-        to ::
-        vehicleCategory ::
-        automatic ::
-        numDoors ::
-        driverAge ::
-        nationality ::
-        HNil =>
-        println(s"pickup location: $pickup")
-    }
+    val reservation: Reservation = Generic[Reservation].from(reservationH)
+    println(s"pickup location: ${reservation.pickup}")
   }
 
 }
