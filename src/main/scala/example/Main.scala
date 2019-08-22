d2708ddda633e03e54452add929b55433bb9c9e6
Toby Hobson <toby.hobson@gmail.com>
2019-08-22 14:07:44 +0200

    Dropping Hlist types and using Generic only

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -1,15 +1,9 @@
 package example
 
 import java.time.LocalDate
-import shapeless.{::, HNil, Generic}
+import shapeless.Generic
 
 object Main {
-
-  /* Our HList representation of the case classes */
-  type LocationH = String :: String :: LocalDate :: LocalDate :: HNil
-  type VehicleH = String :: Boolean :: Int :: HNil
-  type DriverH = Int :: String :: HNil
-  type ReservationH = String :: String :: LocalDate :: LocalDate :: String :: Boolean :: Int :: Int :: String :: HNil
 
   def main(args: Array[String]): Unit = {
     // Our case class representations of the data
@@ -17,13 +11,12 @@ object Main {
     val vehicle = Vehicle(vehicleCategory = "Economy", automatic = false, numDoors = 4)
     val driver = Driver(driverAge = 35, nationality = "British")
 
-    // HList representations
-    val locationH: LocationH = Generic[Location].to(location)
-    val vehicleH: VehicleH = Generic[Vehicle].to(vehicle)
-    val driverH: DriverH = Generic[Driver].to(driver)
+    val locationRepr = Generic[Location].to(location)
+    val vehicleRepr = Generic[Vehicle].to(vehicle)
+    val driverRepr = Generic[Driver].to(driver)
 
-    val reservationH: ReservationH = locationH ++ vehicleH ++ driverH
-    val reservation: Reservation = Generic[Reservation].from(reservationH)
+    val reservationRepr = locationRepr ++ vehicleRepr ++ driverRepr
+    val reservation: Reservation = Generic[Reservation].from(reservationRepr)
     println(s"pickup location: ${reservation.pickup}")
   }
 
